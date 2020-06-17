835514dd36a6f552b685fe584c2fbca1fe98760c
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-06-17 13:30:22 -0300

    Transformed into functional component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import classes from './Burger.css'
 
 // Redux connection
@@ -7,24 +7,22 @@ import { connect } from 'react-redux'
 // Components
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
-class Burger extends Component {
-    render() {
-        const ingredients = Object.keys(this.props.ings).map(ingredient => {
-            return [...Array(this.props.ings[ingredient])].map((_, i) =>
-                <BurgerIngredient type={ingredient} key={Math.random()} />
-            )
-        }).reduce(
-            (allIngredients, currentIngredient)=>allIngredients.concat(currentIngredient),
-            []
+function Burger(props) {
+    const ingredients = Object.keys(props.ings).map(ingredient => {
+        return [...Array(props.ings[ingredient])].map(() =>
+            <BurgerIngredient type={ingredient} key={Math.random()} />
         )
-        return (
-            <div className={classes.Burguer}>
-                <BurgerIngredient type='bread-top' />
-                {ingredients.length > 0 ? ingredients : <p>Start adding ingredients!</p>}
-                <BurgerIngredient type='bread-bottom' />
-            </div>
-        )
-    }
+    }).reduce(
+        (allIngredients, currentIngredient) => allIngredients.concat(currentIngredient),
+        []
+    )
+    return (
+        <div className={classes.Burguer}>
+            <BurgerIngredient type='bread-top' />
+            {ingredients.length > 0 ? ingredients : <p>Start adding ingredients!</p>}
+            <BurgerIngredient type='bread-bottom' />
+        </div>
+    )
 }
 
 function mapStateToProps(state) {
